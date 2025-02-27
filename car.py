032f62ee12e39d2c6fe2bc0384558e9460cd4ab9
Rosendo Marquez <120551095+Buenexity@users.noreply.github.com>
2025-02-27 16:18:33 -0600

    Update car.py
    Pwm functionality to prevent car for swinging too fast. Aswell as making to so both motors experience power to some extent when turning left. For example while turning left right motor is set to "speed" variable while left is at half power.

--- car.py
+++ car.py
@@ -1,6 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 
+#Pwm 
+
+Pwm_Speed = 60
+
 # Motor Pins
 # Left motor (Motor IN1 - Pin 17, Motor IN2 - Pin 18)
 Motor_IN4 = 17
@@ -10,14 +14,14 @@ Motor_IN3 = 18
 Motor_IN2 = 23
 Motor_IN1 = 24
 
-#Pwm_ENA = 12
-#Pwm_ENB = 13
+Pwm_ENA = 12
+Pwm_ENB = 13
 
 GPIO.setmode(GPIO.BCM)  # Set GPIO pin mode
 
 #pwm pins
-#GPIO.setup(Pwm_ENA, GPIO.OUT)
-#GPIO.setup(Pwm_ENB, GPIO.OUT)
+GPIO.setup(Pwm_ENA, GPIO.OUT)
+GPIO.setup(Pwm_ENB, GPIO.OUT)
 
 #setup pins
 GPIO.setup(Motor_IN1, GPIO.OUT)
@@ -27,16 +31,21 @@ GPIO.setup(Motor_IN4, GPIO.OUT)
 
 
 #PWM pins @ 100hz frequency
-#pwm_motor_1 = GPIO.PWM(Pwm_ENA, 100) 
-#pwm_motor_2 = GPIO.PWM(Pwm_ENB, 100)
+pwm_motor_1 = GPIO.PWM(Pwm_ENA, 100) 
+pwm_motor_2 = GPIO.PWM(Pwm_ENB, 100)
 
 #start the motors @ 0% duty cycle
-#pwm_motor_1.start(0)
-#pwm_motor_2.start(0)
+pwm_motor_1.start(0)
+pwm_motor_2.start(0)
+
+pwm_motor_1.ChangeDutyCycle(Pwm_Speed)
+pwm_motor_2.ChangeDutyCycle(Pwm_Speed)
 
 #Motor Functions
 
 def MoveForward():
+    pwm_motor_1.ChangeDutyCycle(speed)       
+    pwm_motor_2.ChangeDutyCycle(speed)
     GPIO.output(Motor_IN1, GPIO.HIGH)  # Left motor forward
     GPIO.output(Motor_IN2, GPIO.LOW)   
     GPIO.output(Motor_IN3, GPIO.HIGH)  # Right motor forward
@@ -45,6 +54,8 @@ def MoveForward():
 
 
 def MoveBackward():
+    pwm_motor_1.ChangeDutyCycle(speed)       
+    pwm_motor_2.ChangeDutyCycle(speed)
     GPIO.output(Motor_IN1, GPIO.LOW)   # Left motor backward
     GPIO.output(Motor_IN2, GPIO.HIGH)  
     GPIO.output(Motor_IN3, GPIO.LOW)   # Right motor backward
@@ -52,6 +63,8 @@ def MoveBackward():
     print("back")
 
 def TurnRight():
+    pwm_motor_1.ChangeDutyCycle(speed)       
+    pwm_motor_2.ChangeDutyCycle(speed // 2)
     GPIO.output(Motor_IN1, GPIO.LOW)   
     GPIO.output(Motor_IN2, GPIO.HIGH)  # Left motor reverse
     GPIO.output(Motor_IN3, GPIO.HIGH)  
@@ -59,6 +72,8 @@ def TurnRight():
     print("Right")
 
 def TurnLeft():
+    pwm_motor_1.ChangeDutyCycle(speed//2)       
+    pwm_motor_2.ChangeDutyCycle(speed)
     GPIO.output(Motor_IN1, GPIO.HIGH)  # Left motor forward
     GPIO.output(Motor_IN2, GPIO.LOW)   
     GPIO.output(Motor_IN3, GPIO.LOW)   # Right motor stop
@@ -66,6 +81,8 @@ def TurnLeft():
     print("Left")
 
 def StopMotors():
+    pwm_motor_1.ChangeDutyCycle(0)       
+    pwm_motor_2.ChangeDutyCycle(0)
     GPIO.output(Motor_IN1, GPIO.LOW)   
     GPIO.output(Motor_IN2, GPIO.LOW)   
     GPIO.output(Motor_IN3, GPIO.LOW)   
