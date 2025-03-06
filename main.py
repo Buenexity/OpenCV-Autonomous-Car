4d87902ccb9070035886ba61aa6a361284be5106
Brandon Shin <bmshin02@gmail.com>
2025-03-06 09:37:48 -0800

    temp comment out

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-from car import TurnLeft, MoveForward, TurnRight
+#from car import TurnLeft, MoveForward, TurnRight
 import cv2
 import imageProcessing as imp
 cap = cv2.VideoCapture(0)
 
-while True:
-    if cap.isOpened(): 
-        ret, frame = cap.read()
-    if not cap.isOpened():
-        print("Cannot open camera")
-        image_col = cv2.imread('./images/track.jpg')
+# while True:
+#     if cap.isOpened(): 
+#         ret, frame = cap.read()
+#     if not cap.isOpened():
+#         print("Cannot open camera")
+#         image_col = cv2.imread('./images/track.jpg')
             
          
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-    carOffset = imp.FindOffset(frame, ret)
-    if (carOffset < -40):
-        TurnLeft()
-        print("turn left")
-    elif (carOffset > 40):
-        TurnRight()
-        print("turn right")
-    else:
-        MoveForward()
-        print("go straight")
+#     # if frame is read correctly ret is True
+#     if not ret:
+#         print("Can't receive frame (stream end?). Exiting ...")
+#     carOffset = imp.FindOffset(frame, ret)
+#     if (carOffset < -40):
+#         TurnLeft()
+#         print("turn left")
+#     elif (carOffset > 40):
+#         TurnRight()
+#         print("turn right")
+#     else:
+#         MoveForward()
+#         print("go straight")
     
-    if cv2.waitKey(1) == ord('q'):
-        break
+#     if cv2.waitKey(1) == ord('q'):
+#         break
 
 speedLimit = imp.SpeedLimitDetection()
 
-print(f"Speed Limit: {speedLimit}")
+#print(f"Speed Limit: {speedLimit}")
 
 cv2.destroyAllWindows()
 
